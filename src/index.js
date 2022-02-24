402bc75812cec231ff31c1e85705957845752d03
maxim-kolmykov <96133680+maxim-kolmykov@users.noreply.github.com>
2022-02-24 05:50:14 +0300

    feat: add JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    let open = [];
+    let obj = {};
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        open.push(bracketsConfig[i][0]);
+        obj[bracketsConfig[i][1]] = bracketsConfig[i][0];
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        let lastEl = stack[stack.length - 1];
+        if (open.includes(str[i])) {
+
+            if (stack.length !== 0) {
+                let top = stack[stack.length - 1]
+                if (obj[str[i]] == top) {
+                    stack.pop()
+                } else {
+                    stack.push(str[i])
+                }
+            } else {
+                stack.push(str[i])
+            }
+        } else { //закрывает
+            if (stack.length === 0) {
+                return false
+            }
+
+            let top = stack[stack.length - 1]
+            if (obj[str[i]] == top) {
+                stack.pop()
+            }
+        }
+    }
+
+    return stack.length === 0;
 }
